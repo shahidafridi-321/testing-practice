97e94f10f39faa6c06c76de0de5e5349a80fee2b
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-22 17:52:18 +0500

    create functionality that pass tests of z

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -4,8 +4,18 @@ export function caesarCipher(string, shiftFactor) {
 	let caesarCipherString = "";
 	for (let i = 0; i < string.length; i++) {
 		caesarCipherString += String.fromCharCode(
-			string.charCodeAt(i) + shiftFactor
+			string.charCodeAt(i) + shiftFactor >= 122
+				? string.charCodeAt(i) + shiftFactor - 26
+				: string.charCodeAt(i) + shiftFactor
 		);
 	}
 	return caesarCipherString;
 }
+
+/* let z = "z";
+let Z = "Z";
+let charCodeOfz = z.charCodeAt(0);
+let charCodeOfZ = Z.charCodeAt(0);
+console.log(charCodeOfz, charCodeOfZ);
+console.log(String.fromCharCode(charCodeOfz - 25));
+ */
