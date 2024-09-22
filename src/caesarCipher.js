37149e9ba5f9e2cd8eade4bb2315a9de462eeb3b
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-22 17:34:48 +0500

    create functionality pass test of abc to def/ABC to DEF

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,4 +1,11 @@
 export function caesarCipher(string, shiftFactor) {
 	if (typeof string === "undefined" || typeof shiftFactor === "undefined")
 		return "Invalid input";
+	let caesarCipherString = "";
+	for (let i = 0; i < string.length; i++) {
+		caesarCipherString += String.fromCharCode(
+			string.charCodeAt(i) + shiftFactor
+		);
+	}
+	return caesarCipherString;
 }
