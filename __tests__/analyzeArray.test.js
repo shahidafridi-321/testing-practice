8e6a46a74f65fa5eb6d1e47ba30473ba6b619244
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-22 20:23:24 +0500

    write different edge tests for average

--- __tests__/analyzeArray.test.js
+++ __tests__/analyzeArray.test.js
@@ -7,3 +7,20 @@ test("Handles empty parameters", () => {
 test("Handles empty array", () => {
 	expect(analyzeArray([])).toBe("Invalid input");
 });
+
+test("Average of [1,2,3,4,5] is 3", () => {
+	let obj = analyzeArray([1, 2, 3, 4, 5]);
+	expect(obj.average).toBe(3);
+});
+
+test("Average of [1,2,3,4,5,6,10] is 4.428", () => {
+	let obj = analyzeArray([1, 2, 3, 4, 5,6,10]);
+	expect(obj.average).toBeCloseTo(4.428);
+});
+
+test("Average of [1.1,2.6,3.9,4.1,5,6,10] is 4.67", () => {
+	let obj = analyzeArray([1.1,2.6,3.9,4.1,5,6,10]);
+	expect(obj.average).toBeCloseTo(4.67);
+});
+
+
--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,8 +1,17 @@
 function isEmpty(array) {
-	if (!array) return true;
+	if (typeof array === "undefined") return true;
 	return false;
 }
+
 export function analyzeArray(array) {
 	if (isEmpty(array)) return "Invalid input";
-  if(array.length === 0) return "Invalid input";
+	if (array.length === 0) return "Invalid input";
+	let average = array.reduce((total, cur) => total + cur, 0) / array.length;
+	let max = 0;
+	let min = 0;
+	let length = array.length;
+
+	return {
+		average,
+	};
 }
