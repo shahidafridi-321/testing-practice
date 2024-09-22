cdff7a184bd99de56b2d3844a51ee1cf85bef890
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-22 17:51:43 +0500

    create test that for xyz to abc

--- __tests__/caesarCipher.test.js
+++ __tests__/caesarCipher.test.js
@@ -11,3 +11,16 @@ test("Converts abc to def", () => {
 test("Converts ABC to DEF", () => {
 	expect(caesarCipher("ABC", 3)).toBe("DEF");
 });
+
+test("Converts HeLLo to KhOOr", () => {
+	expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+});
+
+test("Converts xyz to abc", () => {
+	expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+
+test("Converts z to c", () => {
+	expect(caesarCipher("z", 3)).toBe("c");
+});
+
