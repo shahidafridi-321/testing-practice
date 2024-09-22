e113d90813cb92cbf97945a1cb7bf8564f264266
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-22 17:30:15 +0500

    create tests abc expects DEF

--- __tests__/caesarCipher.test.js
+++ __tests__/caesarCipher.test.js
@@ -3,3 +3,11 @@ import { caesarCipher } from "../src/caesarCipher";
 test("Handles empty parameters", () => {
 	expect(caesarCipher()).toBe("Invalid input");
 });
+
+test("Converts abc to def", () => {
+	expect(caesarCipher("abc", 3)).toBe("def");
+});
+
+test("Converts ABC to DEF", () => {
+	expect(caesarCipher("ABC", 3)).toBe("DEF");
+});
